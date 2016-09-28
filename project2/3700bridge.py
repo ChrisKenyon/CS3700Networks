a49617e890f1545f732da19d37f14fc249da91c2
Eric <eric.cc.su@gmail.com>
2016-09-28 11:26:56 -0400

    BPDU comparison and bridge updating

--- project2/3700bridge.py
+++ project2/3700bridge.py
@@ -33,8 +33,8 @@ class BPDU:
 
 class Bridge:
     ports = []
-    neighbors = []
-    last_received = {}
+    forward_table = {} # dict where key, value pairs are of <neighbor_id>: <port #>
+    last_received = {} # dict where key, value pairs are of <neighbor_id>: <time.time stamp>
     last_sent = None
     def __init__(self, bridge_id, LANs):
         self.bridge_id = bridge_id
@@ -52,16 +52,34 @@ class Bridge:
             self.sockets.append(s)
         print "Bridge " + id + " starting up\n"
 
+    def update_root(self, bpdu):
+        data = json.loads(bpdu)
+        self.root_id = data.get("root")
+        self.cost_to_root = data.get("cost") + 1
+
     def broadcast(self):
         for sock in self.sockets:
             bpdu = BPDU(self.bridge_id, self.root_id, self.cost_to_root)
             sock.send(str(bpdu))
 
     def receive(self, port, bpdu):
-        # TODO do we need port? Tracking which neighbor to which port?
         data = json.loads(bpdu)
         last_received[data.get("source")] = time.time()
+        self.forward_table[data.get("source")] = port
 
+        # Using if/else algorithm defined in "Comparing BPDUs" slide of Bridging class slides
+        # If cost to root from source < current cost to root, set to new root
+        if self.int(data.get("root"), 16) < int(self.root_id, 16):
+            self.update_root(bpdu)
+        # else if IDs are same but cost from neighbor < self.cost, set new root
+        elif self.int(data.get("root"), 16) == int(self.root_id, 16) and (data.get("cost") < self.cost_to_root):
+            self.update_root(bpdu)
+        # Else if costs are same, roots are same, but self.id > neighbor.id, update to go through neighbor
+        elif self.int(data.get("root"), 16) == int(self.root_id, 16) and (data.get("cost") == self.cost_to_root) and self.int(data.get("source"), 16) < int(self.bridge_id, 16):
+            self.update_root(bpdu)
+        # Else do nothing
+        else:
+            pass
 
 if __name__ == "__main__":
         id = sys.argv[1]
@@ -74,7 +92,7 @@ if __name__ == "__main__":
                 # Calls select with all the sockets; change the timeout value (1)
                 ready, ignore, ignore2 = select.select(sockets, [], [], 1)
 
-                # Reads from each fo the ready sockets
+                # Reads from each of the ready sockets
                 for x in ready:
                         data = x.recv(1500)
                         print(data)
